b269841796dd9c5e436997bcf9f02f0cbb7a6ac7
Ibrahim-Abdullahi-Borle <ibroabdullahi254@gmail.com>
2025-05-14 01:42:24 -0700

    java-script-third-lesson

---
+++ index.js
@@ -0,0 +1,18 @@
+let firstfavNum= 74;
+let secondfavNum=7;
+
+console.log(firstfavNum+ secondfavNum);
+console.log(firstfavNum-secondfavNum);
+console.log(firstfavNum*secondfavNum);
+console.log(firstfavNum/secondfavNum);
+console.log(firstfavNum%secondfavNum);
+
+// relational operators
+console.log(20 > 20);
+console.log(20 < 20);
+console.log(20 >= 20);
+console.log(20 <= 20);
+
+// equality oprators
+console.log(20 === 20);
+console.log(20 !== 20);
